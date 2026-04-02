88a3e90e7fba8894121710dee1e48e11cfa1d52a
welldone <wildan.alfarin2007@gmail.com>
2026-04-02 12:06:25 +0700

    Add Comments

--- app/src/main/java/com/example/resapras/SplashActivity.kt
+++ app/src/main/java/com/example/resapras/SplashActivity.kt
@@ -8,15 +8,10 @@ import com.example.resapras.LoginScreen
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        // Install splash screen
-        val splashScreen = installSplashScreen()
-
-        // Set keep on screen condition
-        splashScreen.setKeepOnScreenCondition { false }
-
         super.onCreate(savedInstanceState)
-
-        // Langsung pindah ke LoginScreen
+        val splashScreen = installSplashScreen()
+        //Kontrol SplashScreen
+        splashScreen.setKeepOnScreenCondition { false }
         startActivity(Intent(this, LoginScreen::class.java))
         finish()
     }
